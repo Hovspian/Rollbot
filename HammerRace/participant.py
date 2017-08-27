898c434509986c91325438e3384b22e01c385e2b
aha65 <nepharus@gmail.com>
2017-08-26 22:56:09 -0700

    Participant class has been moved to its own file.

---
+++ HammerRace/participant.py
@@ -0,0 +1,22 @@
+import random
+
+
+class Participant:
+    """Race participant"""
+    def __init__(self):
+        self.progress = 0
+        self.min_move = 3
+        self.max_move = 13
+        self.nametag = ''
+        self.title = ''
+
+    def set_nametag(self, name):
+        self.nametag = name
+
+    def set_title(self, title):
+        self.title = title
+
+    def make_move(self, bonus = 0):
+        min_move = self.min_move + bonus
+        roll = random.randint(min_move, self.max_move)
+        self.progress += roll
