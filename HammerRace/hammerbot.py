814f1e28e53ee509141d8dcfc8203184d33907fd
aha65 <nepharus@gmail.com>
2017-08-27 22:03:03 -0700

    Moved announcements to the Announcement class and broke down some more functions.

--- HammerRace/hammerbot.py
+++ HammerRace/hammerbot.py
@@ -1,5 +1,6 @@
 from HammerRace.race import Race
 from HammerRace.participant import Participant
+from HammerRace.announcements import Announcement
 
 
 class HammerRaceManager:
@@ -7,6 +8,7 @@ class HammerRaceManager:
 
     def __init__(self):
         self.race = Race()
+        self.announcement = Announcement(self.race)
         self.race_in_progress = False
 
     def init_race(self):
@@ -17,11 +19,12 @@ class HammerRaceManager:
         self.y = self.init_participant(short_name='y', name='Yes')
         self.n = self.init_participant(short_name='n', name='No')
         self.h = self.init_participant(short_name='h', name=':hammer:')
+        self.announcement.overriding_winner = self.h
 
         self.race.set_num_participants()
 
     def init_participant(self, short_name, name):
-        participant = Participant(short_name=short_name, name=name)
+        participant = Participant(short_name, name)
         self.race.participants.append(participant)
         return participant
 
@@ -33,63 +36,8 @@ class HammerRaceManager:
         if len(self.race.winners) > 0:
             self.race_in_progress = False
 
-    def report_round(self):
-        """String representing the entire round"""
-        participant_slots = []
+    def round_report(self):
+        return self.announcement.round_report()
 
-        for participant in self.race.participants:
-            if participant in self.race.winners:
-                display_winner = self.string_winner_path(participant)
-                participant_slots.append(display_winner)
-            else:
-                display_in_progress = self.string_progress_path(participant)
-                participant_slots.append(display_in_progress)
-
-        race_track = self.race.get_race_track()
-        report = race_track.format(*participant_slots)
-        return report
-
-    def string_winner_path(self, participant: Participant):
-        path = '|'
-        for i in range(0, self.race.distance_to_finish):
-            path += ' '
-        path += '| {} |'.format(participant.short_name)
-        return path
-
-    def string_progress_path(self, participant: Participant):
-        path = '|'
-        for i in range(0, participant.progress):
-            path += '~'
-        path += participant.short_name
-
-        steps_left = self.race.steps_left(participant.progress)
-        for j in range(0, steps_left):
-            path += ' '
-        path += "|   |"
-        return path
-
-    def announce_winner(self):
-        if self.h in self.race.winners:
-            return 'The answer is {}'.format(self.h.name)
-        elif len(self.race.winners) > 1:
-            return 'The answer is maybe'
-
-        winner_name = self.race.winners[0].name
-        return 'The answer is {}'.format(winner_name)
-
-    def announce_gold_owed(self):
-        message = ''
-        for participant in self.race.participants:
-            if participant not in self.race.winners:
-                steps_remaining = self.race.steps_left(participant.progress)
-                gold = self.calculate_gold(steps_remaining)
-                message += '{} owes {} gold.\n'.format(participant.short_name, gold)
-
-        return message
-
-    def calculate_gold(self, steps_remaining):
-        return pow(steps_remaining, 2) * 3 + 100
-
-
-class Announcer:
-    """ TODO hold all methods related to representing the racetrack and announcing progress"""
+    def winner_report(self):
+        return self.announcement.winner() + '\n' + self.announcement.gold_owed()
