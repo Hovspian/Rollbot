8854e4550a250c9675689a7439916d919f0e0af0
aha65 <nepharus@gmail.com>
2017-08-31 19:29:18 -0700

    HammerRaceManager super class now resides in its own file.

--- HammerRace/hammerbot.py
+++ HammerRace/hammerbot.py
@@ -1,44 +1,8 @@
-from HammerRace.race import Race
-from HammerRace.participant import Participant
-from HammerRace.announcements import Announcement
-
-
-class HammerRaceManager:
-    """Manage relationship between participants and race"""
-
-    def __init__(self):
-        self.race = Race()
-        self.announcement = Announcement(self.race)
-        self.race_in_progress = True
-
-    def init_participants(self):
-        self.race.set_num_participants()
-
-    def init_participant(self, short_name, name):
-        participant = Participant(short_name, name)
-        self.race.add_participant(participant)
-        return participant
-
-    def next_round(self):
-        for participant in self.race.participants:
-            participant.make_move()
-            if self.race.check_winner(participant.progress):
-                self.race.add_winner(participant)
-        self.check_race_end()
-
-    def check_race_end(self):
-        if len(self.race.winners) > 0:
-            self.race_in_progress = False
-
-    def round_report(self):
-        return self.announcement.round_report()
-
-    def winner_report(self):
-        return self.announcement.gold_owed()
+from HammerRace.hammermanager import HammerRaceManager
 
 
 class ClassicHammer(HammerRaceManager):
-    """TODO might have its own announcer?"""
+    # TODO might have its own announcer?
 
     def __init__(self):
         super().__init__()
@@ -82,7 +46,7 @@ class ComparisonHammer(HammerRaceManager):
         self.options = options
 
     def unique_short_name(self):
-        """TODO select unique short_names for everything that comes through"""
+        # TODO select unique short_names for everything that comes through
         return True
 
     def init_participants(self):
