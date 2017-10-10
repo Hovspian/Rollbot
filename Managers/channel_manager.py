170f11b13526e80213daa056b83b068c47a1fb1e
Hammerlord <nepharus@gmail.com>
2017-10-09 20:02:05 -0700

    games_in_progress method renamed to the more accurate active_games.

--- Managers/channel_manager.py
+++ Managers/channel_manager.py
@@ -5,7 +5,7 @@ class ChannelManager:
     # Applies the restriction of one ongoing game per channel.
 
     def __init__(self, bot):
-        self.games_in_progress = {}
+        self.active_games = {}
         self.bot = bot
 
     async def is_valid_channel_host(self, ctx):
@@ -29,8 +29,8 @@ class ChannelManager:
 
     def get_game(self, ctx):
         channel = ctx.message.channel
-        if channel in self.games_in_progress:
-            return self.games_in_progress[channel]
+        if channel in self.active_games:
+            return self.active_games[channel]
 
     async def check_valid_new_game(self, ctx):
         channel = ctx.message.channel
@@ -41,27 +41,27 @@ class ChannelManager:
 
     def add_game_in_session(self, ctx, game):
         channel = ctx.message.channel
-        self.games_in_progress[channel] = game
+        self.active_games[channel] = game
 
     async def add_user_to_game(self, channel, user):
-        self.games_in_progress[channel].add_user(user)
+        self.active_games[channel].add_user(user)
         await self.bot.say("{} joined the game.".format(user.display_name))
 
     def vacate_channel(self, ctx):
         channel = ctx.message.channel
-        self.games_in_progress.pop(channel)
+        self.active_games.pop(channel)
 
     def is_game_in_channel(self, channel):
-        return channel in self.games_in_progress.keys()
+        return channel in self.active_games.keys()
 
     def is_game_in_progress(self, channel):
         if self.is_game_in_channel(channel):
-            game = self.games_in_progress[channel]
+            game = self.active_games[channel]
             return game.in_progress
 
     def is_user_in_game(self, channel, user):
         if self.is_game_in_channel(channel):
-            game = self.games_in_progress[channel]
+            game = self.active_games[channel]
             return user in game.players
 
     def is_invalid_user_error(self, channel, user):
@@ -69,7 +69,7 @@ class ChannelManager:
         if not self.is_game_in_channel(channel):
             error = "No game in this channel."
         elif self.is_game_in_progress(channel):
-            error = "It's too late to join."
+            error = "Sign ups are closed for this game."
         elif self.is_user_in_game(channel, user):
             error = "{} is already in the game.".format(user.display_name)
         return error
