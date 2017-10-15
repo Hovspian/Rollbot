3de9e43def812fda8ee833fd406d1c15ab1e03d5
Hammerlord <nepharus@gmail.com>
2017-10-15 16:57:53 -0700

    Add AvatarHandler structures for Blackjack.

--- player_avatar.py
+++ player_avatar.py
@@ -1,5 +1,44 @@
+from typing import List
+
+
 class PlayerAvatar:
+
     """ Couples Discord member objects and their avatar within a game """
     def __init__(self, user, avatar: any):
         self.user = user
+        self.display_name = user.display_name
         self.avatar = avatar
+        self.afk = 0
+
+
+class AvatarHandler:
+
+    """ Interprets the PlayerAvatar structure """
+
+    @staticmethod
+    def create_avatar(user, avatar) -> PlayerAvatar:
+        return PlayerAvatar(user, avatar)
+
+    @staticmethod
+    def get_avatar(player: PlayerAvatar) -> any:
+        return player.avatar
+
+    @staticmethod
+    def get_name(player: PlayerAvatar) -> str:
+        return player.display_name
+
+
+class BlackjackAvatarHandler(AvatarHandler):
+
+    """ Method names in Blackjack for clarity """
+
+    @staticmethod
+    def create_avatar(user, hand) -> PlayerAvatar:
+        return PlayerAvatar(user, [hand])
+
+    @staticmethod
+    def get_first_hand(player: PlayerAvatar) -> any:
+        return player.avatar[0]
+
+    def get_hands(self, player: PlayerAvatar) -> List[any]:
+        return self.get_avatar(player)
