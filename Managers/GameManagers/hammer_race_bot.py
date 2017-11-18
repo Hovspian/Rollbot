4c61b1fb4d57edb560acb1a9e91be045aac0254d
Hammerlord <nepharus@gmail.com>
2017-11-17 22:56:18 -0800

    Remove unneeded gold division. Add typehints to hammer_race_bot.

--- Managers/GameManagers/hammer_race_bot.py
+++ Managers/GameManagers/hammer_race_bot.py
@@ -1,28 +1,29 @@
 import asyncio
 from HammerRace.hammer_modes import *
 from Managers.GameManagers.game_manager import GameManager
+from Managers.data_manager import SessionDataManager
 
 
 class HammerRaceBot(GameManager):
     def __init__(self, bot):
         super().__init__(bot)
 
-    def create_askhammer(self, ctx):
+    def create_askhammer(self, ctx) -> ClassicHammer:
         askhammer = ClassicHammer(ctx)
         self.add_game(askhammer)
         return askhammer
 
-    def create_comparisonhammer(self, ctx):
+    def create_comparisonhammer(self, ctx) -> ComparisonHammer:
         comparison_hammer = ComparisonHammer(ctx)
         self.add_game(comparison_hammer)
         return comparison_hammer
 
-    def create_versushammer(self, ctx):
+    def create_versushammer(self, ctx) -> VersusHammer:
         versus_hammer = VersusHammer(ctx)
         self.add_game(versus_hammer)
         return versus_hammer
 
-    async def run(self, hammer_race):
+    async def run(self, hammer_race) -> None:
         if hammer_race.valid_num_participants():
             await self._say_start_message(hammer_race)
             await self._run_race(hammer_race)
@@ -30,17 +31,17 @@ class HammerRaceBot(GameManager):
         else:
             await self.bot.say(hammer_race.invalid_participants_error)
 
-    async def _say_setup_message(self, ctx):
+    async def _say_setup_message(self, ctx) -> None:
         host_name = ctx.message.author.display_name
         setup_message = f"{host_name} is starting a race. Type /join in the next 20 seconds to join."
         await self.bot.say(setup_message)
 
-    async def _say_start_message(self, hammer_race):
+    async def _say_start_message(self, hammer_race) -> None:
         start_message = hammer_race.get_start_message()
         if start_message:
             await self.bot.say(start_message)
 
-    async def _run_race(self, hammer_race):
+    async def _run_race(self, hammer_race) -> None:
         hammer_race.in_progress = True
         await self.bot.say(hammer_race.round_report())
 
@@ -53,19 +54,17 @@ class HammerRaceBot(GameManager):
 
 
 class HammerPayoutHandler:
-    def __init__(self, game: VersusHammer, data_manager):
+    def __init__(self, game: VersusHammer, data_manager: SessionDataManager):
         self.game = game
         self.data_manager = data_manager
 
-    def resolve_payouts(self):
+    def resolve_payouts(self) -> None:
         for loser in self.game.losers:
             gold_amount = loser['gold']
             divided_amount = loser['divided_gold']
             self.pay_winners(divided_amount)
             self.data_manager.update_gold(loser, -gold_amount)
 
-    def pay_winners(self, gold_amount):
-        num_winners = len(self.game.winners)
-        divided_gold = gold_amount // num_winners
+    def pay_winners(self, gold_amount) -> None:
         for winner in self.game.winners:
-            self.data_manager.update_gold(winner, divided_gold)
+            self.data_manager.update_gold(winner, gold_amount)
