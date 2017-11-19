04dda6e4c5403616fca9915439c4ebd9ee09ffc2
Hammerlord <nepharus@gmail.com>
2017-11-19 11:22:36 -0800

    Create a new parent class NewGame that asks for ctx.

--- joinable_game_class.py
+++ joinable_game_class.py
@@ -1,19 +1,31 @@
 from player_avatar import PlayerAvatar
 
 
-class JoinableGame:
+class NewGame:
     def __init__(self, ctx):
         self.ctx = ctx
         self.host = ctx.message.author
         self.host_name = self.host.display_name
         self.users = []  # All users who join a game
+
+    def add_user(self, user) -> None:
+        self.users.append(user)
+
+    def get_context(self) -> object:
+        # Info such as who started the game, what channel
+        return self.ctx
+
+
+class JoinableGame(NewGame):
+    def __init__(self, ctx):
+        super().__init__(ctx)
         self.players = []  # List[PlayerAvatar]
         self.in_progress = False
         self.max_time_left = 180
         self.add_user(self.host)
 
     def add_user(self, user) -> None:
-        self.users.append(user)
+        super().add_user(user)
         avatar = self.get_avatar(user)
         self.add_player(user, avatar)
 
@@ -25,7 +37,3 @@ class JoinableGame:
     def get_avatar(self, user) -> any:
         # Method that constructs the user's in-game representation
         pass
-
-    def get_context(self) -> object:
-        # Info such as who started the game, what channel
-        return self.ctx
